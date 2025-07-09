583941c60e4b6b63e51f6df37d2ffea54baf3086
Muhammet Fişek <muhammetfisek121@gmail.com>
2025-07-09 19:18:53 +0300

    Projelerim bileşeninde yeni projeler eklendi ve mevcut projelerin başlıkları, açıklamaları ile görsel alanları güncellendi. Ayrıca, her proje için yeni fotoğraflar eklendi ve GitHub bağlantıları güncellendi.

--- mf_site/src/projelerim/Projelerim.jsx
+++ mf_site/src/projelerim/Projelerim.jsx
@@ -20,6 +20,23 @@ import spor3 from "../fotolar/spor3.jpeg";
 import staj1 from "../fotolar/staj1.jpeg";
 import staj2 from "../fotolar/staj2.jpeg";
 import staj3 from "../fotolar/staj3.jpeg";
+import newssite1 from "../fotolar/newssite1.jpeg";
+import newssite2 from "../fotolar/newssite2.jpeg";
+import newssite3 from "../fotolar/newssite3.jpeg";
+import hesapmakinesi1 from "../fotolar/hesapmakinesi1.jpeg";
+import hesapmakinesi2 from "../fotolar/hesapmakinesi2.jpeg";
+import yapılacak1 from "../fotolar/yapılacak1.jpeg";
+import yapılacak2 from "../fotolar/yapılacak2.jpeg";
+import gardener1 from "../fotolar/gardener1.jpeg";
+import gardener2 from "../fotolar/gardener2.jpeg";
+import gardener3 from "../fotolar/gardener3.jpeg";
+import oyun1 from "../fotolar/oyun1.jpeg";
+import oyun2 from "../fotolar/oyun2.jpeg";
+import oyun3 from "../fotolar/oyun3.jpeg";
+import oyun4 from "../fotolar/oyun4.jpeg";
+
+
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
@@ -54,7 +71,7 @@ const projeler = [
       { ad: "Windows Forms", renk: "linear-gradient(90deg,#43e97b,#38f9d7)" },
     ],
     fotolar: [goruntuisleme1, goruntuisleme2, goruntuisleme3],
-    github: "#",
+    github: "https://github.com/muhammetfisek/image-processing.git",
   },
   // 3. KUTU
   {
@@ -67,7 +84,7 @@ const projeler = [
       { ad: "SQLite", renk: "linear-gradient(90deg,#00bcd4,#4caf50)" },
     ],
     fotolar: [spor1,spor2,spor3],
-    github: "#",
+    github: "https://github.com/muhammetfisek/Spor-Kompleksi.git",
   },
   // 4. KUTU
   {
@@ -81,95 +98,94 @@ const projeler = [
 
     ],
     fotolar: [staj1,staj2,staj3],
-    github: "#",
+    github: "https://github.com/muhammetfisek/staj-projesi.git",
   },
   // 5. KUTU
   {
-    baslik: "Görüntü İşleme ve Kümeleme Uygulaması",
+    baslik: "Statik Web Sitesi: Haberler",
     aciklama:
-      "C# Windows Forms ile geliştirilmiş kapsamlı görüntü işleme uygulaması.",
+      "HTML ve CSS kullanılarak geliştirildi. Haber ve içerik gösteren bir web sitesi oluşturuldu. Anasayfa,Bilim,Spor,Teknoloji vb. sayfalardan oluşmaktadır.",
     etiketler: [
-      { ad: "C#", renk: "linear-gradient(90deg,#1976d2,#00e6d6)" },
-      { ad: "Windows Forms", renk: "linear-gradient(90deg,#43e97b,#38f9d7)" },
+      { ad: "HTML", renk: "linear-gradient(90deg,#1976d2,#00e6d6)" },
+      { ad: "CSS", renk: "linear-gradient(90deg,#43e97b,#38f9d7)" },
     ],
-    fotolar: [hakkimda_pp, site_icon, pp],
-    github: "#",
+    fotolar: [newssite2,newssite1,newssite3],
+    github: "https://github.com/muhammetfisek/News-WebSite.git",
   },
   // 6. KUTU
   {
-    baslik: "Işık Yoğunluğu Ölçer",
+    baslik: "Horse-Racing-Game",
     aciklama:
-      "BH1750 ışık yoğunluğu sensörünü ESP8266 NodeMCU kartı ve Arduino IDE kullanarak ışık şiddetini ölçme uygulaması.",
+      "C# kullanılarak geliştirilmiştir. Oyuncu sayısı girilerek oyuna başlanır. Minimum 2 , maximum 4 oyuncu kabul edilir. Atlar rastgele ilerler ve bitiş çizgisine ilk ulaşan oyuncu kazanır. Oyun reset ile tekrar başlar.",
     etiketler: [
-      { ad: "SwiftUI", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
-      { ad: "C++", renk: "linear-gradient(90deg,#2196f3,#19e6d6)" },
-      { ad: "Arduino", renk: "linear-gradient(90deg,#00c853,#00e6d6)" },
-      { ad: "ESP8266", renk: "linear-gradient(90deg,#00bcd4,#4caf50)" },
+      { ad: "C#", renk: "linear-gradient(90deg,#1976d2,#00e6d6)" },
+      { ad: "Windows Forms", renk: "linear-gradient(90deg,#43e97b,#38f9d7)" },
     ],
     fotolar: [pp, hakkimda_pp, site_icon],
-    github: "#",
+    github: "https://github.com/muhammetfisek/Horse-Racing-Game.git",
   },
   // 7. KUTU
   {
-    baslik: "x",
+    baslik: "Stock-Tracking-System",
     aciklama:
-      "Bu uygulama, kuaförler ve güzellik salonları için randevu yönetim uygulamasıdır. Backend kısmı Spring Boot ile yazılmıştır. Yapay zeka ise Python-Flask ile geliştirilmiştir. Yapay zekaya fotoğrafınızı yükleyerek saç kesimi ve bakım önerileri alabilxy .",
+      "C# ve MS-SQL kullanılarak geliştirilmiştir. Yönetici ve çalışan olmak üzere iki ayrı panel bulunmaktadır. Yönetici giriş yapabilir ve çalışan ekleyebilir. Çalışan giriş yaptıktan sonra ürün ekleyip çıkararak ürün stok takip sistemi yapar.",
     etiketler: [
-      { ad: "Python-Flask", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
-      { ad: "Spring Boot", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
+      { ad: "C#", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
+      { ad: "MS-SQL", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
     ],
     fotolar: [berber1, berber2, berber3],
-    github: "#",
+    github: "https://github.com/muhammetfisek/Stock-Tracking-System.git",
   },
   // 8. KUTU
   {
-    baslik: "Görüntü İşleme ve Kümeleme Uygulaması",
+    baslik: " Hesap Makinesi",
     aciklama:
-      "C# Windows Forms ile geliştirilmiş kapsamlı görüntü işleme uygulaması.",
+      "HTML, CSS ve JavaScript kullanarak geliştirdiğim tam işlevli bir web tabanlı hesap makinesi uygulamasıdır. Temel aritmetik işlemleri destekler ve dinamik tema değiştirme özelliğine sahiptir.",
     etiketler: [
-      { ad: "C#", renk: "linear-gradient(90deg,#1976d2,#00e6d6)" },
-      { ad: "Windows Forms", renk: "linear-gradient(90deg,#43e97b,#38f9d7)" },
+      { ad: "JavaScript", renk: "linear-gradient(90deg,#1976d2,#00e6d6)" },
+      { ad: "HTML", renk: "linear-gradient(90deg,#43e97b,#38f9d7)" },
+      { ad: "CSS", renk: "linear-gradient(90deg,#00bcd4,#4caf50)" },
     ],
-    fotolar: [hakkimda_pp, site_icon, pp],
-    github: "#",
+    fotolar: [hesapmakinesi1,hesapmakinesi2],
+    github: "https://github.com/muhammetfisek/HesapMakinesi.git",
   },
   // 9. KUTU
   {
-    baslik: "Işık Yoğunluğu Ölçer",
+    baslik: "To-Do-List",
     aciklama:
-      "BH1750 ışık yoğunluğu sensörünü ESP8266 NodeMCU kartı ve Arduino IDE kullanarak ışık şiddetini ölçme uygulaması.",
+      "Bu projede  yapılacaklar listesi uygulaması geliştirilmiştir. Kullanıcılar yapılacakları ekleyebilir, düzenleyebilir ve silebilir. Ayrıca Tümü, Devam Ediyor ve Bitti butonları ile görevleri filtreleyebilirler.",
     etiketler: [
-      { ad: "SwiftUI", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
-      { ad: "C++", renk: "linear-gradient(90deg,#2196f3,#19e6d6)" },
-      { ad: "Arduino", renk: "linear-gradient(90deg,#00c853,#00e6d6)" },
-      { ad: "ESP8266", renk: "linear-gradient(90deg,#00bcd4,#4caf50)" },
+      { ad: "JavaScript", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
+      { ad: "HTML-CSS", renk: "linear-gradient(90deg,#2196f3,#19e6d6)" },
+      { ad: "Bootstrap", renk: "linear-gradient(90deg,#00c853,#00e6d6)" },
+      { ad: "JQuery", renk: "linear-gradient(90deg,#00bcd4,#4caf50)" },
     ],
-    fotolar: [pp, hakkimda_pp, site_icon],
-    github: "#",
+    fotolar: [yapılacak1,yapılacak2],
+    github: "https://github.com/muhammetfisek/To-Do-List.git",
   },
   // 10. KUTU
   {
-    baslik: "y",
+    baslik: "Statik Web Sitesi: Gardener",
     aciklama:
-      "Bu uygulama, kuaförler ve güzellik salonları için randevu yönetim uygulamasıdır. Backend kısmı Spring Boot ile yazılmıştır. Yapay zeka ise Python-Flask ile geliştirilmiştir. Yapay zekaya fotoğrafınızı yükleyerek saç kesimi ve bakım önerileri alabilxy .",
+      "Bu site HTML, CSS, JS kullanılarak geliştirilmiştir. Site, bazı bitkilerin nasıl yetiştirildiği ve bakımının nasıl yapıldığı hakkında faydalı bilgiler içermektedir.",
     etiketler: [
-      { ad: "Python-Flask", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
-      { ad: "Spring Boot", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
+      { ad: "JavaScript", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
+      { ad: "HTML-CSS", renk: "linear-gradient(90deg,#2196f3,#19e6d6)" },
     ],
-    fotolar: [berber1, berber2, berber3],
-    github: "#",
+    fotolar: [gardener1,gardener2,gardener3],
+    github: "https://github.com/muhammetfisek/gardener.git",
   },
   // 11. KUTU
   {
-    baslik: "Görüntü İşleme ve Kümeleme Uygulaması",
+    baslik: "Mini-Game-WebSite",
     aciklama:
-      "C# Windows Forms ile geliştirilmiş kapsamlı görüntü işleme uygulaması.",
+      "HTML, CSS ve vanilla JavaScript ile geliştirdiğim, kullanıcı dostu bir mini oyun portalıdır. Pong, Yılan Oyunu ve Köstebeğe Vurma gibi popüler oyunları içeren bu site, temel front-end becerilerimi ve dinamik oyun mantığı uygulama yeteneğimi kapsamlı bir şekilde sergiler",
     etiketler: [
-      { ad: "C#", renk: "linear-gradient(90deg,#1976d2,#00e6d6)" },
-      { ad: "Windows Forms", renk: "linear-gradient(90deg,#43e97b,#38f9d7)" },
+      { ad: "JavaScript", renk: "linear-gradient(90deg,#3ea6ff,#00e6d6)" },
+      { ad: "HTML-CSS", renk: "linear-gradient(90deg,#2196f3,#19e6d6)" },
     ],
-    fotolar: [hakkimda_pp, site_icon, pp],
-    github: "#",
+    fotolar: [oyun1,oyun2,oyun3,oyun4],
+    github: "https://github.com/muhammetfisek/Mini-Game-Website.git",
   },
   // 12. KUTU
   {
